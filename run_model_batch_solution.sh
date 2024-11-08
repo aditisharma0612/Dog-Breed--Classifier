1c6edc7cf5487574a78c62f221ffd2bbf6c57971
aditisharma0612 <143523593+aditisharma0612@users.noreply.github.com>
2024-11-08 19:04:13 +0530

    Create run_model_batch_solution.sh

---
+++ run_model_batch_solution.sh
@@ -0,0 +1,3 @@
+python check_images_solution.py --dir pet_images/ --arch resnet  --dogfile dognames.txt > resnet_solution.txt
+python check_images_solution.py --dir pet_images/ --arch alexnet  --dogfile dognames.txt > alexnet_solution.txt
+python check_images_solution.py --dir pet_images/ --arch vgg  --dogfile dognames.txt > vgg_solution.txt
